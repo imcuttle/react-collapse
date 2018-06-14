4d7c32d8daf904f63ebf7b61cd8a6477ea5ba3e6
imcuttle <moyuyc95@gmail.com>
2018-06-14 21:02:14 +0800

    fix: the height is not correct when content has margin-top or margin-bottom

---
+++ example/App/Margin.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Collapse} from '../../src';
+
+
+export class Margin extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {isOpened: false};
+  }
+
+
+  render() {
+    const {isOpened} = this.state;
+
+    return (
+      <div>
+        <div className="config">
+          <label className="label">
+            Opened:
+            <input
+              className="input"
+              type="checkbox"
+              checked={isOpened}
+              onChange={({target: {checked}}) => this.setState({isOpened: checked})} />
+          </label>
+        </div>
+
+        <Collapse isOpened={isOpened}>
+          <div style={{backgroundColor: 'red', marginTop: 100, height: 100}}/>
+        </Collapse>
+      </div>
+    );
+  }
+}
